a8c2dcb48ddb2282b1e3840e95a1b2d8b8107671
Flávio Marques <flavio.fm.marques@gmail.com>
2017-02-07 03:36:49 +0000

    now presenting data with a touch of colours, click to open article in safari

---
+++ STV_TestApp/STV_TestApp/ArticleTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  ArticleTableViewCell.h
+//  STV_TestApp
+//
+//  Created by Flávio Marques on 07/02/17.
+//  Copyright © 2017 com.prototypes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ArticleTableViewCell : UITableViewCell
+
+@end
---
+++ STV_TestApp/STV_TestApp/ArticleTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  ArticleTableViewCell.m
+//  STV_TestApp
+//
+//  Created by Flávio Marques on 07/02/17.
+//  Copyright © 2017 com.prototypes. All rights reserved.
+//
+
+#import "ArticleTableViewCell.h"
+
+@implementation ArticleTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
